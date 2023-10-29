9e13bfd7a5d62da3092d05294356a08db6357280
Pontus Doverstav <doverstav@gmail.com>
2023-10-29 15:54:15 +0100

    WIP todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 import { next as A } from "@automerge/automerge";
@@ -10,19 +8,45 @@ import {
   useRemoteAwareness,
 } from "@automerge/automerge-repo-react-hooks";
 
+interface Todo {
+  id: string;
+  task: string;
+  done: boolean;
+}
+
 interface CounterDoc {
   counter: A.Counter;
   list: Array<string>;
   myObject: Record<string, string>;
+  todos: Todo[];
 }
 
 interface EphemeralState {
-  clicks: 0;
+  username: string;
+  todosCreated: number;
+  todosCompleted: number;
+  todosUncompleted: number;
+  todosDeleted: number;
 }
 
 interface AppProps {
   userId: string;
 }
+
+interface ExtendedArray<T> extends Array<T> {
+  insertAt(index: number, ...args: T[]): ExtendedArray<T>;
+  deleteAt(index: number, numDelete?: number): ExtendedArray<T>;
+}
+
+/**
+ * TODO
+ * MUST:
+ * Setup ephemeral state
+ *
+ * Nice to have
+ * Move controls below todo description in mobile
+ * Allow editing todos after they have been created?
+ * */
 
 function App({ userId }: AppProps) {
   // Setup document here
@@ -33,9 +57,7 @@ function App({ userId }: AppProps) {
       const handle = repo.create<CounterDoc>();
       // Set initial values
       handle.change((d) => {
-        d.counter = new A.Counter();
-        d.list = [];
-        d.myObject = {};
+        d.todos = [];
       });
       return handle;
     },
@@ -57,9 +79,89 @@ function App({ userId }: AppProps) {
     localUserId: userId,
   });
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const task = formData.get("task");
+
+    if (task || task !== "") {
+      const todo: Todo = {
+        id: crypto.randomUUID(),
+        task: task as string,
+        done: false,
+      };
+
+      changeDoc((d) => d.todos.push(todo));
+      event.currentTarget.reset();
+    }
+  };
+
   return (
     <>
+      <h1>ToDo (better title required)</h1>
+      <p>
+        A collaborative todo app using CRDTs (via{" "}
+        <a href="https://automerge.org">automerge</a>) to communicate between
+        peers.
+      </p>
       <div>
+        <form className="input-container" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="todo-input"
+            name="task"
+            placeholder="Todo..."
+          />
+          <button type="submit">Add todo</button>
+        </form>
+      </div>
+      <hr className="separator" />
+      <div>
+        {doc?.todos.map((todo, i) => (
+          <div
+            className={`todo-container ${todo.done ? "todo-completed" : ""}`}
+            key={`${todo.id}-${i}`}
+          >
+            <p className="todo-description">{todo.task}</p>
+            <label htmlFor={`${todo.id}-done`}>Done</label>
+            <input
+              id={`${todo.id}-done`}
+              type="checkbox"
+              onClick={() =>
+                changeDoc((d) => {
+                  const todoIndex = d.todos.findIndex(
+                    (el) => el.id === todo.id
+                  );
+                  const oldTodo = d.todos[todoIndex];
+                  d.todos[todoIndex] = { ...oldTodo, done: !oldTodo.done };
+                })
+              }
+            />
+            <button
+              className="button-delete"
+              onClick={() =>
+                changeDoc((d) => {
+                  const index = d.todos.findIndex((el) => el.id === todo.id);
+                  (d.todos as ExtendedArray<Todo>).deleteAt(index);
+                })
+              }
+            >
+              Delete
+            </button>
+          </div>
+        ))}
+      </div>
+      <div className="peers-container">
+        <div className="peers-heading">
+          Connected peers: {Object.keys(peerStates).length}
+        </div>
+        <button className="peers-button">
+          <div className="down">{">"}</div>
+        </button>
+      </div>
+
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -106,7 +208,7 @@ function App({ userId }: AppProps) {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   );
 }
