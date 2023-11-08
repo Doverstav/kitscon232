a8cabf0b78556195e3ba2b130f5d78e51f7679ba
Pontus Doverstav <doverstav@gmail.com>
2023-11-08 21:34:49 +0100

    Add BroadcastChannel as fallback if websocket server is down

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import App from "./App.tsx";
 import "./index.css";
 
 import { Repo } from "@automerge/automerge-repo";
-//import { BroadcastChannelNetworkAdapter } from "@automerge/automerge-repo-network-broadcastchannel";
+import { BroadcastChannelNetworkAdapter } from "@automerge/automerge-repo-network-broadcastchannel";
 import { BrowserWebSocketClientAdapter } from "@automerge/automerge-repo-network-websocket";
 import { IndexedDBStorageAdapter } from "@automerge/automerge-repo-storage-indexeddb";
 import { RepoContext } from "@automerge/automerge-repo-react-hooks";
 
 const repo = new Repo({
-  network: [new BrowserWebSocketClientAdapter("wss://sync.automerge.org")],
+  network: [
+    new BroadcastChannelNetworkAdapter(),
+    new BrowserWebSocketClientAdapter("wss://sync.automerge.org"),
+  ],
   storage: new IndexedDBStorageAdapter(),
 });
 
